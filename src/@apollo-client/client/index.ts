1d450ebef92b33f39b900e6db92e6c67633938fd
velimihaylov <veli.mihaylov@deptagency.com>
2021-02-18 17:59:22 +0000

    Using env var for the graphql endpoint

--- src/@apollo-client/client/index.ts
+++ src/@apollo-client/client/index.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
 
+const graphqlEndpoint =
+  process.env.REACT_APP_GRAPHQL_ENDPOINT || 'http://localhost:4444'
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4444/graphql'
+  uri: `${graphqlEndpoint}/graphql`
 })
 
 export const client = new ApolloClient({
